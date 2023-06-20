7ce23162ec4b293cddb5046a42af9bc3dea3cbd2
ShrugalTayal <shrugaltayal@gmail.com>
2023-06-20 12:29:33 +0530

    Create HelloWorldJNI.java

---
+++ MyJNIProject/src/com/jni/HelloWorldJNI.java
@@ -0,0 +1,34 @@
+public class HelloWorldJNI {
+    static {
+        // Load the native library
+        System.loadLibrary("HelloWorldJNI");
+    }
+
+    // Native method declaration
+    private native void sayHello();
+
+    private native void startProbe();
+
+    private native void stopProbe();
+
+    private native void addToCollection();
+
+    private native void deleteFromCollection();
+
+    public static void main(String[] args) {
+        // Create an instance of HelloWorldJNI and invoke sayHello method
+        new HelloWorldJNI().sayHello();
+
+        // Create an instance of HelloWorldJNI and invoke startProbe method
+        new HelloWorldJNI().startProbe();
+
+        // Create another instance of HelloWorldJNI and invoke startProbe method
+        new HelloWorldJNI().startProbe();
+
+        // Create another instance of HelloWorldJNI and invoke addToCollection method
+        new HelloWorldJNI().addToCollection();
+
+        // Create another instance of HelloWorldJNI and invoke deleteFromCollection method
+        new HelloWorldJNI().deleteFromCollection();
+    }
+}
